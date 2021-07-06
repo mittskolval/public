474aec63cce6501ea7bfd9505fa98bf75e41a5ca
Niclas Lindstedt <niclaslindstedt@users.noreply.github.com>
2021-07-06 14:38:22 +0200

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -14,7 +14,7 @@ main() {
     && echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu focal stable" | \
       sudo tee /etc/apt/sources.list.d/docker.list > /dev/null \
     && sudo apt-get update \
-    && sudo apt-get install docker-ce docker-ce-cli containerd.io \
+    && sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose \
     && sudo addgroup $USER docker
 }
 
